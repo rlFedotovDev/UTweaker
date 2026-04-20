a82b110f2ea606e115864f650846eb0b298088b0
MarkAdderly <makuadarloh@gmail.com>
2026-04-20 16:31:31 -0400

    Удалён мусор

--- UpdateBuildNumber.ps1
+++ UpdateBuildNumber.ps1
@@ -1,14 +1,5 @@
-﻿# Путь к файлу с номером сборки
-$buildFilePath = "$PSScriptRoot\BuildNumber.txt"
-
-# Чтение текущего номера сборки
+﻿$buildFilePath = "$PSScriptRoot\BuildNumber.txt"
 $currentBuildNumber = [int](Get-Content $buildFilePath)
-
-# Увеличение номера сборки
 $currentBuildNumber++
-
-# Запись нового номера сборки обратно в файл
 $currentBuildNumber | Set-Content $buildFilePath
-
-# Установка номера сборки в свойство MSBuild
 Write-Host "##vso[task.setvariable variable=BuildNumber]$currentBuildNumber"
